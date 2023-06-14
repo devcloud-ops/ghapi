f79a7b20fe2d7ca3050decac13d7aa1add49881b
Muhammad Faisal Siddiqui <61173238+devcloud-ops@users.noreply.github.com>
2023-06-14 12:07:11 +1000

    Update create-python-env.sh

--- create-python-env.sh
+++ create-python-env.sh
@@ -1,1 +1,12 @@
+python -m pip install pygithub --user
+
+pip show pygithub
+pip3 show pygithub
+
+#--------------------------------------------#
+
+https://github.com/pyenv/pyenv#installation
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+cd ~/.pyenv && src/configure && make -C src
+
 
