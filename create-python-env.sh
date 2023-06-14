5dcffa040e37adab42b70362d91a19c3ec1f2f25
Muhammad Faisal Siddiqui <61173238+devcloud-ops@users.noreply.github.com>
2023-06-14 12:12:25 +1000

    Update create-python-env.sh

--- create-python-env.sh
+++ create-python-env.sh
@@ -9,4 +9,29 @@ https://github.com/pyenv/pyenv#installation
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 cd ~/.pyenv && src/configure && make -C src
 
+curl https://pyenv.run | bash
+
+# Setup your shell environment for Pyenv
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH=$PATH:$PYENV_ROOT
+command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -v)"
+
+exec "$SHELL"
+
+pyenv install 3.10.4
+pyenv install 3.6.9
+pyenv install 2.7.17
+pyenv install 2.7.5
+
+which python
+virtualenv venv -p `which python`
+virtualenv venv -p `which python3`
+
+virtualenv --python=/usr/bin/python2.6 <path/to/myvirtualenv>
+
+pip install virtualenvwrapper
+
+
+
 
